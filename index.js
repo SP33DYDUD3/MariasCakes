5daddfd27c51a99b1e61a85af6bea698e17e16fe
SP33DYDUD3 <142846504+SP33DYDUD3@users.noreply.github.com>
2025-06-30 12:19:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function showSidebar() {
+        document.querySelector(".sidebar").classList.add("show");
+    }
+
+    function hideSidebar() {
+        document.querySelector(".sidebar").classList.remove("show");
+    }
